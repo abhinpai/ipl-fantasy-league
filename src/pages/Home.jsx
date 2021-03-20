8c725cdf99dd5eef078acb6fcca361cfeff74fad
Abhin Pai <abhinpai96@gmail.com>
2021-03-21 01:22:04 +0530

    Impl not support div for small resolution devices

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,22 +2,19 @@ import React from 'react';
 import Question from '../components/compounds/Question';
 import Schedule from '../components/compounds/Schedule';
 import ScroreBoard from '../components/compounds/ScroreBoard';
-import { actionShowSchedule, actionUserLogin } from '../state/action';
-import useAppData from '../state/dataLayer';
 
 function Home() {
-  const [{ isScheduleDivOpen }, dispatch] = useAppData();
   return (
-    <main className='main-page'>
-     <Schedule/>
-     <Question/>
-     <ScroreBoard/>
-      {/* <div className='match-event-question'>*</div>
-      <div className='score-board'>*</div>
-      <button onClick={() => actionShowSchedule(!isScheduleDivOpen, dispatch)}>
-        {isScheduleDivOpen ? 'Hide' : 'Show'}
-      </button> */}
-    </main>
+    <>
+      <main className='main-page'>
+        <Schedule />
+        <Question />
+        <ScroreBoard />
+      </main>
+      <div className='not-support'>
+        <p>Sorry, This webapp does'nt support on any small screen device</p>
+      </div>
+    </>
   );
 }
 
