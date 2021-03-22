3032138c8c0e85447f7396e9ced2a595c441fc5c
Abhin Pai <abhinpai96@gmail.com>
2021-03-22 13:16:53 +0530

    Cosmetic changes to the toast notification and added the validation to signup

---
+++ src/assets/icons/index.js
@@ -0,0 +1,5 @@
+import SuccerssIcon from './success.svg';
+import ErrorIcon from './error.svg';
+import WarningIcon from './warning.svg';
+
+export { SuccerssIcon, ErrorIcon, WarningIcon };
--- src/components/molecules/Notification.jsx
+++ src/components/molecules/Notification.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import { ToastContainer, toast, Slide } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
+import { SuccerssIcon, ErrorIcon, WarningIcon } from '../../assets/icons/index';
 
 export const successToast = (content) =>
-  toast.success(toastBody(content, 'success.svg'));
+  toast.success(toastBody(content, SuccerssIcon));
 
 export const warningToast = (content) =>
-  toast.warning(toastBody(content, 'warning.svg'));
+  toast.warning(toastBody(content, WarningIcon));
 
 export const errorToast = (content) =>
-  toast.error(toastBody(content, 'error.svg'));
+  toast.error(toastBody(content, ErrorIcon));
 
 const toastBody = ({ title, message }, img) => (
   <div className='notification'>
-    {/* <img src={require(`../../assets/icons/${img}`)} alt='' /> */}
+    <img src={img} alt='Notification Icon' />
     <div>
       <h3>{title}</h3>
       {message && <p>{message}</p>}
