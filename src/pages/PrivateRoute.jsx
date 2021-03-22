07e153db483297be590a15da5caa748c8392d3fe
Abhin Pai <abhinpai96@gmail.com>
2021-03-22 15:52:03 +0530

    Implemeted the signin logic

--- src/pages/PrivateRoute.jsx
+++ src/pages/PrivateRoute.jsx
@@ -1,14 +1,35 @@
 import React from 'react';
 import { Redirect, Route } from 'react-router';
 import useAppData from '../state/dataLayer';
+import { localStorageKeys } from '../utils/constants';
 import Home from './Home';
 
 const PrivateRoute = () => {
-  const [{ isLoggedIn }] = useAppData();
+  const [{ loggedsUser }] = useAppData();
+
+  const isUserLoggedIn = () => {
+    const user = localStorage.getItem(localStorageKeys.user);
+    let localUser = null;
+
+    if (user) {
+      localUser = JSON.parse(user);
+    }
+
+    if (
+      localUser &&
+      loggedsUser &&
+      loggedsUser?.emailId === localUser?.emailId &&
+      loggedsUser?.name === localUser?.name
+    ) {
+      return true;
+    }
+    return false;
+  };
+
   return (
     <Route
       render={({ location }) =>
-        isLoggedIn ? (
+        isUserLoggedIn() ? (
           <Home />
         ) : (
           <Redirect
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,28 +1,100 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PlayersImage from '../assets/players.svg';
 import ArrowIcon from '../assets/icons/arrow-right.svg';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import useAppData from '../state/dataLayer';
 import { actionUserLogin } from '../state/action';
+import { errorToast, successToast } from '../components/molecules/Notification';
+import useFirebase, { generateFirebaseUserId } from '../utils/firebaseUtil';
+import { Collections, localStorageKeys } from '../utils/constants';
+
+const defaulCredential = {
+  emailId: '',
+  password: '',
+};
 
 function SignIn() {
+  const [credential, setCredential] = useState(defaulCredential);
   const [, dispatch] = useAppData();
+  const dbInstance = useFirebase();
+  const history = useHistory();
+
+  const signInUser = () => {
+    if (credential.password === '' || credential.emailId === '') {
+      errorToast({
+        title: 'Invalid user details',
+        message: 'Come on you need to have a valid credential to get in!',
+      });
+    } else {
+      dbInstance
+        .ref(Collections.users)
+        .child(generateFirebaseUserId(credential.emailId))
+        .on('value', (snapshot) => {
+          onSiginIn(snapshot.val());
+        });
+    }
+  };
+
+  const onSiginIn = (snapshot) => {
+    if (
+      snapshot?.emailId !== credential.emailId &&
+      snapshot?.password !== credential.password
+    ) {
+      error();
+    } else if (snapshot?.emailId !== credential.emailId) {
+      error();
+    } else if (credential.password !== snapshot?.password) {
+      error();
+    } else {
+      successToast({
+        title: 'Successfully signed in',
+        message: 'Welcome to the Fantasy League',
+      });
+      const userInfo = JSON.stringify({
+        emailId: snapshot.emailId,
+        name: snapshot.name,
+      });
+      localStorage.setItem(localStorageKeys.user, userInfo);
+      actionUserLogin(JSON.parse(userInfo), dispatch);
+      history.replace('/');
+    }
+    setCredential(defaulCredential);
+  };
+
+  const error = () =>
+    errorToast({
+      title: 'Invalid credentials',
+      message: 'Please try again with vaid credentials',
+    });
 
   return (
     <section className='auth-section'>
       <div className='auth-section__left-col'>
         <h3>Fantasy League</h3>
         <h1>Join an amazing event with us to be a witness of fantasy league</h1>
-        <input required type='email' placeholder='Email Id' /> <br />
+        <input
+          value={credential.emailId}
+          onChange={(e) =>
+            setCredential({ ...credential, emailId: e.target.value })
+          }
+          required
+          type='email'
+          placeholder='Email Id'
+        />{' '}
+        <br />
         <div className='auth-actions'>
-          <input required type='password' placeholder='Password' />
-          <Link
-            to='/'
-            className='primary-btn auth-action__btn'
-            onClick={() => actionUserLogin(true, dispatch)}
-          >
+          <input
+            value={credential.password}
+            onChange={(e) =>
+              setCredential({ ...credential, password: e.target.value })
+            }
+            required
+            type='password'
+            placeholder='Password'
+          />
+          <div className='primary-btn auth-action__btn' onClick={signInUser}>
             <img src={ArrowIcon} alt='Auth Button' />
-          </Link>
+          </div>
         </div>
         <p>
           Don't have an account?
